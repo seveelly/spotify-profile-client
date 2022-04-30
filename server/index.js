c192e3a53322bab87537c453f692d34c920d7f48
Eliud Ogola <51315397+seveelly@users.noreply.github.com>
2022-04-30 23:51:58 +0300

    Update index.js
    env

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-let REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:8888/callback';
-let FRONTEND_URI = process.env.FRONTEND_URI || 'http://localhost:3000';
+let REDIRECT_URI = process.env.REDIRECT_URI ;
+let FRONTEND_URI = process.env.FRONTEND_URI;
 const PORT = process.env.PORT || 8888;
 
 if (process.env.NODE_ENV !== 'production') {
